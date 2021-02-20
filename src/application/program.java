1486192c460f643ebc59fc635513bd44f2b621a0
Argemiro Nepomuceno <pe-miro@hotmail.com>
2021-02-20 20:21:58 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/program.java
+++ src/application/program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -10,6 +12,9 @@ public class program {
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		Seller seller = new Seller(1, "Argemiro", "argemiro@gmail.com", new Date(), 2000.0, obj);
 		System.out.println(seller);
 	}
