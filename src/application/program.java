672371daaeca47213799c7fc8d8d0ee48086b30c
Argemiro Nepomuceno <pe-miro@hotmail.com>
2021-02-20 19:37:16 -0300

    Seller entity class

--- src/application/program.java
+++ src/application/program.java
@@ -1,12 +1,16 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(1, "Argemiro", "argemiro@gmail.com", new Date(), 2000.0, obj);
+		System.out.println(seller);
 	}
 }
